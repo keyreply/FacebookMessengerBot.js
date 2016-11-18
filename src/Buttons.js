225b99afaa5b84d5031077d4a5845052b37e9e2a
Max Xu <deepthought@gmail.com>
2016-11-18 15:11:43 -0800

    Update Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -21,7 +21,7 @@ class Buttons {
         } else if (button.url) {
           buttons.push({type: 'account_link', url: button.url});
         } else {
-          console.log('[fb-msger-bot] Missing url for account linking');
+          throw Error('Missing url for account linking');
         }
       } else if (button.url) {
         buttons.push({type: 'web_url', url: button.url, title: button.text});
